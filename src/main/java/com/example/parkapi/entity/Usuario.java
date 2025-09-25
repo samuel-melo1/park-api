01f6f731b2dcc4aa38a43abdee099b4cf2462b3b
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-09-24 21:55:23 -0300

    initial commit

---
+++ src/main/java/com/example/parkapi/entity/Usuario.java
@@ -0,0 +1,58 @@
+package com.example.parkapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuarios")
+@Getter @Setter @NoArgsConstructor
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "username", nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "role", nullable = false, length = 30)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @Column(name = "criado_por")
+    private String criadoPor;
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENTE
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(getId(), usuario.getId());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                '}';
+    }
+}
